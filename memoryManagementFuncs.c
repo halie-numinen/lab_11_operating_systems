d5b9ef4a00a7771fb4b0c85b1ff4f66f85d1acd4
Kahlan Walcott <166679300+Kahlan-walcott@users.noreply.github.com>
2026-04-03 16:48:13 -0400

    adding the file and the sugested code.

---
+++ memoryManagementFuncs.c
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main () {
+    void GetSystemInfo(LPSYSTEM_INFO lpSystemInfo);
+    SIZE_T VirtualQuery(LPCVOID lpAddress, PMEMORY_BASIC_INFORMATION
+    lpBuffer, SIZE_T dwLength);
+    
+    return 0;
+}
