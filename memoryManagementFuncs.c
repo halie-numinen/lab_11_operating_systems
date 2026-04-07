87660eec3a2423181c8972e5e47409ae8130395e
Kahlan Walcott <166679300+Kahlan-walcott@users.noreply.github.com>
2026-04-07 14:09:02 -0400

    finshed the assingment and fixed some errors.

--- memoryManagementFuncs.c
+++ memoryManagementFuncs.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 int main () {
-    SYSTEM_INFO si;
+    SYSTEM_INFO systemInfo;
     MEMORY_BASIC_INFORMATION memoryInformation;
     SIZE_T size = 1048576; //2^20
 
-    GetSystemInfo(&si);
-    printf("1. Page size: %lu\n", si.dwPageSize);
-    void* ptr = VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-    if (ptr == NULL) {
+    GetSystemInfo(&systemInfo);
+    printf("1. Page size: %lu\n", systemInfo.dwPageSize);
+    void* memoryAddress = VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (memoryAddress == NULL) {
         printf("Allocation failed\n");
         return 1;
     }
     printf("2. Memory allocated for a size of %zu bytes\n", size);
-    if (VirtualQuery(ptr, &memoryInformation, sizeof(memoryInformation))) {
-        printf("3. System Queried to determine the state of the allocated memory.\n");
-        printf("4. The state of the alocated memory: ");
+    if (VirtualQuery(memoryAddress, &memoryInformation, sizeof(memoryInformation))) {
+        printf("3. System queried to determine the state of the allocated memory.\n");
+        printf("4. The state of the allocated memory: ");
         if (memoryInformation.State == MEM_COMMIT) {
             printf("Committed\n");
         }
@@ -30,11 +30,29 @@ int main () {
     else {
         printf("VirtualQuery failed\n");
     }
-    if (!VirtualFree(ptr, 0, MEM_RELEASE)) {
+    if (!VirtualFree(memoryAddress, 0, MEM_RELEASE)) {
         printf("Release failed.\n");
     }
     else {
         printf("5. De-allocating memory\n");
     }
+
+    if (VirtualQuery(memoryAddress, &memoryInformation, sizeof(memoryInformation))) {
+        printf("6. System queried to determine the state of the allocated memory.\n");
+        printf("The state of the allocated memory: ");
+        if (memoryInformation.State == MEM_COMMIT) {
+            printf("Committed\n");
+        }
+        else if (memoryInformation.State == MEM_RESERVE) {
+            printf("Reserved\n");
+        }
+        else if (memoryInformation.State == MEM_FREE) {
+            printf("Freed\n");
+        }
+    }
+    else {
+        printf("VirtualQuery failed\n");
+    }
+    
     return 0;
 }
